724a89b0dda45e143d01eb12f0563e5f8f2441b2
Mauri <iruam_mauri@msn.com>
2022-02-16 14:11:34 -0300

    Delete Dose de correcao.py

--- JavaScript completo do Iniciante ao mestre(2022)/Exercicios/Dose de correcao.py
+++
@@ -1,5 +0,0 @@
-clicemiaAtual = float(input("Clicemia Atual: "))
-valorGlicemicoPretendido = float(input("Valor Glicemico Pretendido: "))
-resistenciaAinsulina = float(input("Resistencia insulinica: "))
-resposta = (clicemiaAtual - valorGlicemicoPretendido) / resistenciaAinsulina
-print(resposta)
